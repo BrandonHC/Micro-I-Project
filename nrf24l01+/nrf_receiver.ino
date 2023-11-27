ae6f2e1d505f96db0b76cc3853c005ed287e13bd
Brandon C <58312670+BrandonHC@users.noreply.github.com>
2023-11-26 19:23:20 -0600

    updated code for receiver data and calculations

--- nrf24l01+/nrf_receiver.ino
+++ nrf24l01+/nrf_receiver.ino
@@ -6,6 +6,9 @@ RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = {0xe1, 0xf0, 0xf0, 0xf0, 0xf0}; //"00001"
 
+float p1Coordinates[2] = { 0, 0 }; //for now, will write to class later.
+float targetAngle, xDist, yDist;
+
 void setup() {
   Serial.begin(9600);
   radio.begin();
@@ -16,8 +19,19 @@ void setup() {
 
 void loop() {
   if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    float p2Coordinates[2];
+    radio.read(&p2Coordinates, sizeof(p2Coordinates));
+    Serial.println("x2: " + String(p2Coordinates[0]) + ", y2: " + String(p2Coordinates[1]));
+
+    yDist = p2Coordinates[1] - p1Coordinates[1];
+    xDist = p2Coordinates[0] - p1Coordinates[0];
+    targetAngle = atan2(yDist, xDist); //radians
+    
+    if (targetAngle < 0) //for [0, PI] range
+      targetAngle += 2 * M_PI;
+
+    targetAngle *= (180/PI);
+
+    Serial.println("Angle: " + String(targetAngle));
   }
 }
